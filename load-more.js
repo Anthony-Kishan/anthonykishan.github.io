2bde09a4747cde713aa576362c3c0ff63f5793a2
Anthony-Kishan <128999086+Anthony-Kishan@users.noreply.github.com>
2023-04-02 23:10:27 +0600

    Delete load-more.js

--- load-more.js
+++
@@ -1,9 +0,0 @@
-const seeMoreBtn = document.getElementById("see-more-btn");
-const contentDiv = document.querySelector(".projects");
-const BtnBg = document.getElementById("see-more-button");
-
-seeMoreBtn.addEventListener("click", () => {
-  contentDiv.style.overflow = "auto";
-  seeMoreBtn.style.display = "none";
-  BtnBg.style.display = "none";
-});
